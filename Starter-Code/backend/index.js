a4e289ee6bec63f273e70d611abbbc8af666121b
amani75 <amoonfathi1998@gmail.com>
2025-02-01 01:45:24 +0300

    login_register

--- Starter-Code/backend/index.js
+++ Starter-Code/backend/index.js
@@ -1,16 +1,21 @@
 const express = require("express");
-require("dotenv").config();
 const cors = require("cors");
-require("./models/db");
-
-//routers
-
+require("dotenv").config();
 
 const app = express();
 
+//routers
+const userRouter = require("./routers/user");
+
+
+
+
+
 //built-in middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
+app.use("/user",userRouter)
 
 // router middleware
 
