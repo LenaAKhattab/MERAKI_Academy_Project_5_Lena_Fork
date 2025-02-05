ce23c93230b6e8653c5c7bc940f6434ba6f56e34
NaghamGhawanmeh <ghawanmehnagham@gmail.com>
2025-02-05 17:59:01 +0300

    Login frontend/Logic

--- Starter-Code/frontend/src/App.js
+++ Starter-Code/frontend/src/App.js
@@ -11,6 +11,7 @@ const App = () => {
   
       <Routes>
         <Route path={"/register"} element={<Register />} />
+        <Route path={"/login"} element={<Login />} />
       </Routes>
     </div>
   );
