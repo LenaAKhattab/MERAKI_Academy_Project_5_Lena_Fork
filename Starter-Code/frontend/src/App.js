1faf71a1e27058e9071eb73a41a8509c1315844b
NaghamGhawanmeh <ghawanmehnagham@gmail.com>
2025-02-06 13:02:18 +0300

    Frontend Starter Code

--- Starter-Code/frontend/src/App.js
+++ Starter-Code/frontend/src/App.js
@@ -2,19 +2,19 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
-
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routers";
 //===============================================================
 
 const App = () => {
-  return (
-    <div>
-  
-      <Routes>
+  return <RouterProvider router={router} />;
+
+  {
+    /* <Routes>
         <Route path={"/register"} element={<Register />} />
         <Route path={"/login"} element={<Login />} />
-      </Routes>
-    </div>
-  );
+      </Routes> */
+  }
 };
 
 export default App;
