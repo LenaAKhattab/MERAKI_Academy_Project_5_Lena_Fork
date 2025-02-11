a5c9afc5ffe595388fbc369697d6f95605183c93
NaghamGhawanmeh <ghawanmehnagham@gmail.com>
2025-02-12 00:09:46 +0300

    getAllOrders/admin:style

--- Starter-Code/frontend/src/components/SideNav/index.js
+++ Starter-Code/frontend/src/components/SideNav/index.js
@@ -1,64 +1,83 @@
-import * as React from 'react';
-import { extendTheme, styled } from '@mui/material/styles';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import DescriptionIcon from '@mui/icons-material/Description';
-import LayersIcon from '@mui/icons-material/Layers';
-import { AppProvider } from '@toolpad/core/AppProvider';
-import { DashboardLayout } from '@toolpad/core/DashboardLayout';
-import { PageContainer } from '@toolpad/core/PageContainer';
-import Grid from '@mui/material/Grid2';
+import * as React from "react";
+import { extendTheme, styled } from "@mui/material/styles";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import DescriptionIcon from "@mui/icons-material/Description";
+import LayersIcon from "@mui/icons-material/Layers";
+import { AppProvider } from "@toolpad/core/AppProvider";
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import { PageContainer } from "@toolpad/core/PageContainer";
+import Grid from "@mui/material/Grid2";
+import {
+  DataGrid,
+  GridActionsCellItem,
+  GridRowModes,
+  GridRowEditStopReasons,
+} from "@mui/x-data-grid";
+import SaveIcon from "@mui/icons-material/Save";
+import CancelIcon from "@mui/icons-material/Close";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import Button from "@mui/material/Button";
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  setOrders,
+  setCollector,
+  setOrderStatus,
+} from "../../redux/reducers/adminOrders";
 
 const NAVIGATION = [
   {
-    kind: 'header',
-    title: 'Main items',
+    kind: "header",
+    title: "Main items",
   },
   {
-    segment: 'dashboard',
-    title: 'Dashboard',
+    segment: "dashboard",
+    title: "Dashboard",
     icon: <DashboardIcon />,
   },
   {
-    segment: 'orders',
-    title: 'Orders',
+    segment: "orders",
+    title: "Orders",
     icon: <ShoppingCartIcon />,
   },
   {
-    kind: 'divider',
+    kind: "divider",
   },
   {
-    kind: 'header',
-    title: 'Analytics',
+    kind: "header",
+    title: "Analytics",
   },
   {
-    segment: 'reports',
-    title: 'Reports',
+    segment: "reports",
+    title: "Reports",
     icon: <BarChartIcon />,
     children: [
       {
-        segment: 'sales',
-        title: 'Sales',
+        segment: "sales",
+        title: "Sales",
         icon: <DescriptionIcon />,
       },
       {
-        segment: 'traffic',
-        title: 'Traffic',
+        segment: "traffic",
+        title: "Traffic",
         icon: <DescriptionIcon />,
       },
     ],
   },
   {
-    segment: 'integrations',
-    title: 'Integrations',
+    segment: "integrations",
+    title: "Integrations",
     icon: <LayersIcon />,
   },
 ];
 
 const demoTheme = extendTheme({
   colorSchemes: { light: true, dark: true },
-  colorSchemeSelector: 'class',
+  colorSchemeSelector: "class",
   breakpoints: {
     values: {
       xs: 0,
@@ -84,20 +103,178 @@ function useDemoRouter(initialPath) {
   return router;
 }
 
-const Skeleton = styled('div')(({ theme, height }) => ({
+const Skeleton = styled("div")(({ theme, height }) => ({
   backgroundColor: theme.palette.action.hover,
   borderRadius: theme.shape.borderRadius,
   height,
   content: '" "',
 }));
 
+function EditToolbar(props) {
+  const { setRows, setRowModesModel } = props;
+}
+
 export default function DashboardLayoutBasic(props) {
+  const dispatch = useDispatch();
+  const [message, setMessage] = useState("");
+  const [rows, setRows] = useState([]);
+
+  const getAllOrders = () => {
+    axios
+      .get("http://localhost:5000/admin/getAllOrders")
+      .then((result) => {
+        console.log(result);
+        dispatch(setOrders(result.data.orders));
+      })
+      .catch((error) => {
+        console.log(error);
+        setMessage(error.response.data.message);
+      });
+  };
+
+  useEffect(() => {
+    getAllOrders();
+  }, []);
+
+  const orders = useSelector((reducers) => reducers.adminOrdersReducer.orders);
+  console.log("orders", orders);
+
+  useEffect(() => {
+    const rowsData = orders.map((order) => ({
+      id: order.order_id,
+      name: `${order.requester_first_name} ${order.requester_last_name}`,
+      status: order.status,
+      predicted_price: order.predicted_price,
+      last_price: order.last_price,
+      collector: `${order.collector_first_name} ${order.collector_last_name}`,
+    }));
+    setRows(rowsData);
+  }, [orders]); 
+
   const { window } = props;
 
-  const router = useDemoRouter('/dashboard');
+  const router = useDemoRouter("/dashboard");
 
-  // Remove this const when copying and pasting into your project.
   const demoWindow = window ? window() : undefined;
+  const [rowModesModel, setRowModesModel] = React.useState({});
+
+  const handleRowEditStop = (params, event) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const handleEditClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleDeleteClick = (id) => () => {
+    setRows(rows.filter((row) => row.id !== id));
+  };
+
+  const handleCancelClick = (id) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = rows.find((row) => row.id === id);
+    if (editedRow.isNew) {
+      setRows(rows.filter((row) => row.id !== id));
+    }
+  };
+
+  const processRowUpdate = (newRow) => {
+    const updatedRow = { ...newRow, isNew: false };
+    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    return updatedRow;
+  };
+
+  const handleRowModesModelChange = (newRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  const columns = [
+    { field: "name", headerName: "Name", width: 180, editable: true },
+
+    {
+      field: "status",
+      headerName: "Status",
+      width: 100,
+      editable: true,
+    },
+    {
+      field: "predicted_price",
+      headerName: "Predicted Price",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "last_price",
+      headerName: "Last Price",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "collector",
+      headerName: "Collector",
+      width: 150,
+      editable: true,
+      type: "singleSelect",
+      valueOptions: ["ahmad", "ali", "sami"],
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      width: 100,
+      cellClassName: "actions",
+      getActions: ({ id }) => {
+        const isInEditMode =
+          rowModesModel[id]?.mode === GridRowModes.Edit || false;
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              sx={{
+                color: "primary.main",
+              }}
+              onClick={handleSaveClick(id)}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
 
   return (
     <AppProvider
@@ -106,43 +283,31 @@ export default function DashboardLayoutBasic(props) {
       theme={demoTheme}
       window={demoWindow}
     >
-      <DashboardLayout>
+      <DashboardLayout
+        sx={{ "& .MuiIconButton-root": { width: "fit-content" } }}
+      >
         <PageContainer>
-          <Grid container spacing={1}>
-            <Grid size={5} />
-            <Grid size={12}>
-              <Skeleton height={14} />
-            </Grid>
-            <Grid size={12}>
-              <Skeleton height={14} />
-            </Grid>
-            <Grid size={4}>
-              <Skeleton height={100} />
-            </Grid>
-            <Grid size={8}>
-              <Skeleton height={100} />
-            </Grid>
-
-            <Grid size={12}>
-              <Skeleton height={150} />
-            </Grid>
-            <Grid size={12}>
-              <Skeleton height={14} />
-            </Grid>
-
-            <Grid size={3}>
-              <Skeleton height={100} />
-            </Grid>
-            <Grid size={3}>
-              <Skeleton height={100} />
-            </Grid>
-            <Grid size={3}>
-              <Skeleton height={100} />
-            </Grid>
-            <Grid size={3}>
-              <Skeleton height={100} />
-            </Grid>
-          </Grid>
+          {router.pathname === "/orders" && (
+            <div>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    editMode="row"
+                    rowModesModel={rowModesModel}
+                    onRowModesModelChange={handleRowModesModelChange}
+                    onRowEditStop={handleRowEditStop}
+                    processRowUpdate={processRowUpdate}
+                    slots={{ toolbar: EditToolbar }}
+                    slotProps={{
+                      toolbar: { setRows, setRowModesModel },
+                    }}
+                  />
+                </Grid>
+              </Grid>
+            </div>
+          )}
         </PageContainer>
       </DashboardLayout>
     </AppProvider>
