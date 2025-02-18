f49cfd8e0703c4514277d939890481e98f9851ad
NaghamGhawanmeh <ghawanmehnagham@gmail.com>
2025-02-19 01:26:16 +0300

    Home Style

---
+++ Starter-Code/frontend/src/components/BecomeOurCustomer/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./style.css"
+const BecomeOurCustomer = () => {
+  return (
+    <div className='become-our-container'>sss</div>
+  )
+}
+
+export default BecomeOurCustomer
